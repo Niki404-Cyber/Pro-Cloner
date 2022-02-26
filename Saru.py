60c18705a1563a510f81cedc552308d109f5fa58
Mr. NIKI <93582194+Niki404-Cyber@users.noreply.github.com>
2022-02-26 14:38:09 +0300

    Create Saru.py

---
+++ Saru.py
@@ -0,0 +1,13 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from librie import bnsbuy
+    bnsbuy()
+elif bit == '32bit':
+    print "Opps Your Device Not Supported"
